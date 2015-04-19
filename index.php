e22d476c63b5e3b15db3563a2e8a470a0316fbd5
Jiaxin Lin <jiaxinlin@dyn-72-33-198-173.uwnet.wisc.edu>
2015-04-19 07:31:04 -0500

    templates

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
 		
 		<div id="api-list">
 			<div class = "row" style = "height: 100%;">
-				<div class="col-md-6 text-center well " style="height: 85%; overflow: auto;">
-					<ul class="list-group" style="width:50%">
+				<div class="col-md-6" style="height: 85%; overflow: auto;">
+					<ul class="list-group  text-center" style="width:50%">
 						<?php
 							if($res = $conn->query("SELECT count(*) as c, category FROM apicrawldata GROUP BY category")) {
 								while ($row = $res->fetch_assoc()) {
