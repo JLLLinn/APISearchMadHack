e1ba9fff0e91d4ec665073b68477f792e28bad37
Jiaxin Lin <jiaxinlin@dyn-72-33-198-173.uwnet.wisc.edu>
2015-04-19 05:52:12 -0500

    templates

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
   </head>
   <body>
   	<div class="container">
@@ -24,6 +25,18 @@
 		</div>
 		
     	<h1 class="text-center">Ap-Eye</h1>
+    	
+    	<div class = "row">
+    		<div class="span4">
+    			<a class="btn btn-lg btn-success" href="#">
+  				<i class="fa fa-apple fa-2x pull-left"></i> Download our iPhone App</a>
+    		</div>
+    			<a class="btn btn-lg btn-success" href="#">
+  				<i class="fa fa-android fa-2x pull-left"></i> Download our Android App</a>
+  			<div class="span8">
+  			</div>
+    	</div>
+    	
     	
     </div>
     <script src="js/jquery-1.11.2.min.js"></script>
