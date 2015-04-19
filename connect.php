ec0335fa2aea562d68d704efc07bf2a8e636dc2d
Jiaxin Lin <jiaxinlin@dyn-72-33-198-173.uwnet.wisc.edu>
2015-04-19 04:05:30 -0500

    sql

--- connect.php
+++ connect.php
@@ -5,8 +5,8 @@
 	$dbname = "APISearch"
 
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-
+	$conn = new mysqli($servername, $username, $password);
+	echo "conn is sucess";
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
