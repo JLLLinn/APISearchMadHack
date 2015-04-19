6b85562e7c4dde757ee57c8afc7070e14f2a493e
Jiaxin Lin <jiaxinlin@dyn-72-33-198-173.uwnet.wisc.edu>
2015-04-19 04:47:20 -0500

    sql

--- connect.php
+++ connect.php
@@ -6,7 +6,6 @@
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
-	echo "conn is sucess";
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
