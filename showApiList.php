bf6be1afb9f065fc170723dfe852a40d801d698c
Jiaxin Lin <jiaxinlin@JiaxindeMacBook-Air.local>
2015-04-20 16:05:55 -0500

    ToDo: Add Ajax Function for displaying apis after categories shown

--- showApiList.php
+++ showApiList.php
@@ -1,4 +1,10 @@
 <?php
+	require_once("connect.php");
 	$q = $_GET["q"];
-	echo $q;
+	$q = preg_replace("/(<span).*(span>)/", "", $str);
+	if($res = $conn->query("SELECT apiname, hyperlink FROM apicrawldata WHERE category='$q'")) {
+		foreach ($res as $row) {
+			echo $row[apiname];
+		}
+	}
 ?>
