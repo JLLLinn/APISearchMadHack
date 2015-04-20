dd3d44a56201a8103b1b431799689a7481ce4f2c
Jiaxin Lin <jiaxinlin@wirelessprvnat-172-17-213-170.near.illinois.edu>
2015-04-20 16:40:14 -0500

    ToDo: Add Ajax Function for displaying apis after categories shown

--- index.php
+++ index.php
@@ -75,8 +75,7 @@
 				</div>
 				
 				<div  class="col-md-6 text-center">
-					<ul id = "list-apis-under-category" class="list-group text-center" style="max-height: 100%; ; padding-left:25%; padding-right:25%; overflow-y:
-scroll;">
+					<ul id = "list-apis-under-category" class="list-group text-center" style="max-height: 600px; ; padding-left:25%; padding-right:25%; overflow:auto;">
 					</ul>
 				</div>
 			</div>
